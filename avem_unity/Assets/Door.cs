a5c2dc50c83ba0fcd3bb66dbfad2e9312cd08819
olvanyr <olvanyr@gmail.com>
2021-01-12 16:03:44 +0100

    smooth transition beeteen room

---
+++ avem_unity/Assets/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public Collider2D triggerCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OpenDoor() 
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
---
+++ avem_unity/Assets/Snapping.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snapping : MonoBehaviour
+{
+
+    public BoxCollider2D boxCollider;
+    public CameraFollow cameraFollow;
+
+    public float boxX;
+    public float boxY;
+    public float halfHeight;
+    public float halfWidth;
+
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxX = boxCollider.size.x;
+        boxY = boxCollider.size.y;
+
+        Camera camera = Camera.main;
+        halfHeight = camera.orthographicSize;
+        halfWidth = camera.aspect * halfHeight;
+
+        minPosition.x = transform.position.x - ((boxX/2) - halfWidth);
+        minPosition.y = transform.position.y - ((boxY/2) - halfHeight);
+        maxPosition.x = transform.position.x + ((boxX/2) - halfWidth);
+        maxPosition.y = transform.position.y + ((boxY/2) - halfHeight);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cameraFollow.minPosition = minPosition;
+            cameraFollow.maxPosition = maxPosition;
+        }
+        
+    }
+}
