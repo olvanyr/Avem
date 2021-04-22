e610c38678c4c4f3b0835105fe1b8162a36a1757
olvanyr <olvanyr@gmail.com>
2021-04-22 16:36:52 +0200

    add more part to the lab room

---
+++ avem_unity/Assets/Scripts/DropingFruit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropingFruit : MonoBehaviour
+{
+    public bool droped;
+    public Rigidbody2D rb;
+
+    private void Awake()
+    {
+
+        rb.simulated = false;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("player enter trigger");
+                rb.simulated = true;
+                Destroy(gameObject);
+
+        }
+    }
+}
